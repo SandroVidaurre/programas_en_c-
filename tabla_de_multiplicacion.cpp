f5b2d12dafdb5cd175592d9b6b48f57a87785650
SandroVidaurre <156030828+SandroVidaurre@users.noreply.github.com>
2024-01-17 20:31:37 -0400

    Add files via upload

---
+++ tabla_de_multiplicacion.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+	
+int main(){
+	//Solicitar al usuario un numero.
+	cout<<"ingrese un numero para la tabla de multiplicacion"<<endl;
+	int numero;
+	cin >> numero;//lee el numero dado por el usuario.
+	//muestra la tabla de multiplicacion del 1 al 10 para el numero. 
+	cout<<"tabla de multiplicar para " <<numero<<":\n";
+	//proceso del 1 al 10 e imprimir la multiplicacion.
+	for (int i = 1; i <= 10; ++i){
+		//mostrar la multiplicacion.
+		cout<<numero<<"x"<<i<<"="<<(numero*i)<<"\n";
+		
+	}
+	return 0;//indica que el programa se ejecuto sin errores.
+}
