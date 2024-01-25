92b40672922dd4c2bbac2aee1ecaeeaba7a63c2f
SandroVidaurre <156030828+SandroVidaurre@users.noreply.github.com>
2024-01-24 21:06:58 -0400

    Add files via upload

---
+++ contador_primos.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int aleatorio() //funcion de numeros aleatorios
+{
+	return rand() % 100;
+}
+bool esImpar(int numero){ // funcion para numeros impares
+	return numero %2!=0;
+}
+int main(int argc, char *argv[]) {
+	//en base a funciones imprimir n numeros impares. n=5
+	int n,c=0,num;
+	//pedir la cantidad de n numeros aleatorios
+	cout<<"generar N numeros Impares,ingrese n:";
+	cin>>n;
+	while(c<n) //bucle de while para la cantidad de numeros aleatorios
+	{
+		num=aleatorio();
+		if(esImpar(num))
+		{
+			c++;
+			cout<<"impar["<<c<<"]:"<<num<<endl;//muestra la cantidad de numeros impares aleatorios
+		}
+	}
+	return 0;
+	
+}
