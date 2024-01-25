2edc95f51df077f5d8151caecc8e3d62f7f82b2a
SandroVidaurre <156030828+SandroVidaurre@users.noreply.github.com>
2024-01-24 21:46:42 -0400

    Add files via upload

---
+++ funcionesxreferencia.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void ingresar(int &n1,int &n2)//referecia de n1,n2
+{
+	cout<<"ingresa un numero: ";
+	cin>>n1;
+	cout<<"ingresa otro numero: ";
+	cin>>n2;
+}
+void sumar(int n1, int n2, int &resultado)//referecia resultado
+{
+	resultado=n1+n2;
+}
+
+int main(int argc, char *argv[]) {
+	int n1,n2,resultado;
+	ingresar(n1,n2);
+	sumar(n1,n2,resultado);
+	cout<<"La suma es: "<<resultado<<endl;
+	return 0;
+}
